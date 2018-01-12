9c97224d457848bf91c634d8525a37c309b91db3
Wayland Morgan <dotwayland@gmail.com>
2018-01-12 18:30:46 +0000

    Removing raw_input in favor of argparse to populate hex value

--- tools/hex2bin.py
+++ tools/hex2bin.py
@@ -1,14 +1,26 @@
 #!/usr/bin/env python
 
+"""Decode user provided HEX string into binary data and write it to a file for analysis"""
+
 __author__ = "Wayland Morgan"
-__description__ = "Decode user provided HEX string into binary data and write it to a file for analysis"
+__email__ = "dotwayland@gmail.com"
 __date__ = "20171206"
 
-hexstr = raw_input('Enter hex string: ')
-filename = raw_input('Enter file name: ')
-extension = ".bin"
-filename = filename + extension
-data = hexstr.decode('hex')
-filehandle = open(filename, "wb")
-filehandle.write(data)
-filehandle.close()
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input", required=True, help="Hexadecimal String")
+    args = parser.parse_args()
+    
+    hexstr = args.input
+    filename = raw_input('Enter file name: ')
+    extension = ".bin"
+    filename = filename + extension
+    data = hexstr.decode('hex')
+    filehandle = open(filename, "wb")
+    filehandle.write(data)
+    filehandle.close()
+
+if __name__ == '__main__':
+    main()
